94ba0931a6a7cf12e98d54a31f8468b463b35e2c
FarhanIsrak18 <shahidinfo.45@gmail.com>
2022-08-22 21:40:05 +0600

    Customer View

---
+++ Advancde.NET-Final-Project-main/ERP.NET/BLL/Services/SalesManagerServices/CustomerServices.cs
@@ -0,0 +1,35 @@
+﻿using BLL.Entities;
+using DAL.DataAccessFactory;
+using DAL.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services.SalesManagerServices
+{
+    public class CustomerServices
+    {
+        public static List<CustomerModel> Get()
+        {
+            var data = DataAccessFactory.CustomerworkDataAccess().Get();
+            var customers = new List<CustomerModel>();
+            foreach (var item in data)
+            {
+                customers.Add(new CustomerModel()
+                {
+                    id = item.id,
+                    name = item.name,
+                    email = item.email,
+                    phone = item.phone,
+                    delivery_point = item.delivery_point,
+                    first_purchase = item.first_purchase,
+                    type =item.type
+
+                });
+            }
+            return customers;
+        }
+    }
+}
---
+++ Advancde.NET-Final-Project-main/ERP.NET/PL/Controllers/SalesManagerController/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using BLL.Entities;
+using BLL.Services.SalesManagerServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+//using System.Web.Http.Cors;
+
+namespace PL.Controllers.SalesManagerController
+{
+    
+    public class CustomerController : ApiController
+    {
+        [Route("api/customers")]
+        [HttpGet]
+        public HttpResponseMessage GetAll()
+        {
+            var data = CustomerServices.Get();
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+    }
+}
